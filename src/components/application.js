5d61a450813ab0fb5c4a64b6e3eceaf25cd1353e
Mohammad Bilal <mohdbilal16@gmail.com>
2023-08-06 11:34:36 +0530

    usecall added

--- src/components/application.js
+++ src/components/application.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useCallback } from 'react';
 import {
   createItem,
   filterItems,
@@ -12,35 +12,39 @@ import MarkAllAsUnpacked from './mark-all-as-unpacked';
 import NewItem from './new-item';
 
 const Application = () => {
-  const [items, setItems] = useState(getInitialItems());
-  const [newItemName, setNewItemName] = useState('');
+  const [items, setItems] = useState(() => getInitialItems());
+  
+  console.log("---", items)
+  // const add = (name) => {
+  //   const item = createItem(name);
+  //   setItems([...items, item]);
+  // };
 
-  const add = (name) => {
+  const add = useCallback((name) => {
     const item = createItem(name);
     setItems([...items, item]);
-  };
+  }, [items])
 
-  const update = (id, updates) => {
+
+  const update = useCallback((id, updates) => {
     setItems(updateItem(items, id, updates));
-  };
+  },[items]);
 
-  const remove = (id) => {
+  const remove = useCallback((id) => {
     setItems(removeItem(items, id));
-  };
+  },[items]);
 
   const unpackedItems = filterItems(items, { packed: false });
   const packedItems = filterItems(items, { packed: true });
 
-  const markAllAsUnpacked = () => {
+  const markAllAsUnpacked = useCallback(() => {
     return setItems(items.map((item) => ({ ...item, packed: false })));
-  };
+  },[]);
 
   return (
     <main className="flex flex-col gap-8 p-8 mx-auto lg:max-w-4xl">
       <Header items={items} />
       <NewItem
-        newItemName={newItemName}
-        setNewItemName={setNewItemName}
         addItem={add}
       />
       <section className="flex flex-col gap-8 md:flex-row">
